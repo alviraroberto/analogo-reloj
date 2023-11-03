53954585f77fb15b008660f36821d9d04af04a57
Roberto Alvira <alvira5@hotmail.com>
2023-11-03 00:24:37 -0400

    terminado-hora-fecha

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
-const secondHand = document.querySelector('[data-hour-hand]');
-const minsHand = document.querySelector('[data-minute-hand]');
-const hourHand = document.querySelector('[data-second-hand]');
+setInterval(setClock, 1000)
+
+const secondHand = document.querySelector('[data-second-hand]');
+const minuteHand = document.querySelector('[data-minute-hand]');
+const hourHand = document.querySelector('[data-hour-hand]');
 
 
+function setClock() {
+    const currentDate = new Date();
+    const secondsRatio = currentDate.getSeconds()/60;
+    const minutesRatio = (secondsRatio + currentDate.getMinutes()) /60;
+    const hoursRatio = (minutesRatio + currentDate.getHours())/12;
 
+    setRotation(secondHand, secondsRatio)
+    setRotation(minuteHand, minutesRatio)
+    setRotation(hourHand, hoursRatio)
+
+}
+
+
+function setRotation(element, rotationRatio) {
+    element.style.setProperty('--rotation', rotationRatio * 360)
+}
+
+setClock()
